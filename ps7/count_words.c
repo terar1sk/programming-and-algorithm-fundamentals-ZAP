84cc7cf513d9da5f852deda991d63caec623e11e
Dmytro Isai <dmytro.isai@student.tuke.sk>
2023-12-13 20:08:53 +0100

    ps7

--- ps7/count_words.c
+++ ps7/count_words.c
@@ -1,54 +1,43 @@
 #include <stdio.h>
 
-void wrt(FILE *file, int num){
-    if(num == 0){
-        fputc('0', file);
-        return;
-    }
-    char digits[10];
-    int a = 0;
-    
-    while(num > 0){
-        digits[a] = '0' + num % 10;
-        num /= 10;
-        a++;
-    }
-    for(int b = a - 1; b >= 0; b--){
-        fputc(digits[b], file);
-    }
-}
-
 int main(int argc, char *argv[]){
     if(argc != 2){
-        printf("Usage: %s <input/output file>\n", argv[0]);
         return 1;
     }
     FILE *file = fopen(argv[1], "r+");
     if(file == NULL){
-        perror("Error opening file");
         return 1;
     }
+    
     int con = 0;
-    int cur;
+    char word[] = "ananas";
     int ind = 0;
-    while((cur = fgetc(file)) != EOF){
-        char lowerChar = (char)tolower(cur);
-        if(lowerChar == "ananas"[ind]){
+    
+    char ch = fgetc(fp);
+    while(ch != EOF){
+        if(ch == word[ind] || ch == word[ind]-32){
             ind++;
-            if("ananas"[ind] == '\0'){
-                con++;
-                ind = 0;
-            }
         }
         else{
             ind = 0;
         }
+        cd = fgetc(file);
+        
+        if(ind == 6){
+            con++;
+            ind = 0;
+        }
     }
-    rewind(file);
-    wrt(file, con);
-    fputc('\n', file); 
     fclose(file);
-
+    
+    for(int a = 10000; a == 1; a /= 10){
+        if(con >= a){
+            print("%d\n",con/a);
+            con -= con/a - a;
+        }
+    }
+    putchar(con + 48);
+    }
     return 0;
 }
 
