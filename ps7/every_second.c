16ca22434b628b3ac004010f857a84d73102c271
Dmytro Isai <dmytro.isai@student.tuke.sk>
2023-12-14 16:15:45 +0100

    ps7

--- ps7/every_second.c
+++ ps7/every_second.c
@@ -4,72 +4,74 @@ int main(int argc, char *argv[]){
     if(argc != 3){
         return 1;
     }
-
-    FILE *inp = fopen(argv[1], "r");
-    if(inp == NULL){
-        return 1;
-    }
-
-    FILE *otp = fopen(argv[2], "w");
-    if(otp == NULL){
-        fclose(inp);
-        return 1;
-    }
-
-    int ss = 0;
+    
+    char com[][6] = {"START\0", "STOP\0"};
+    int con = 0;
+    int all_sec = 0;
     int sec = 0;
-    int wc = 0;
-
-    int cur;
-    while((cur = fgetc(inp)) != EOF){
-        if(ss){
-            if(cur == ' ' || cur == '\n' || cur == '\t'){
-                if(++wc % 2 == 0){
-                    sec = !sec;
-                }
-            }
-            if(sec){
-                fputc(cur, otp);
-            }
+    char buf[5000];
+    char text[200];
+    int tcon = 0;
+    
+    FILE *inp_file = fopen(argv[1], "r");
+    if(inp_file == NULL){
+        return 1;
+    }    
+    
+    char ch = fgetc(inp_file);
+    while(ch != EOF){
+        while(ch != ' ' && ch != EOF){
+            buf[con] = ch;
+            con++;
+            ch = fgetc(inp_file);
         }
-        if(cur == 'S'){
-            int mt = 1;
-            for(int a = 1; a < 5; ++a){
-                cur = fgetc(inp);
-                if(cur != "START"[a]){
-                    mt = 0;
+        buf[con] = ' ';
+        con++;
+        buf[con] = '\0';
+        if(all_sec == 1){
+            all_sec = 0;
+            for(int a = 0; a < 4; a++){
+                if(buf[a] != com[1][a] || buf[5] != '\0'){
+                    all_sec = 1;
                     break;
                 }
             }
-            if(mt){
-                ss = 1;
+        }
+        if(all_sec == 1){
+            if(sec == 1){
+                for(int a = 0; a < con; a++){
+                    text[tcon] = buf[a];
+                    tcon++;
+                }
                 sec = 0;
             }
             else{
-                fputc('S', otp);
+                sec = 1;
+                text[tcon] = '\0';
             }
         }
-        else if(cur == 'S' && ss){
-            int mt = 1;
-            for(int a = 1; a < 4; ++a){
-                cur = fgetc(inp);
-                if(cur != "STOP"[a]){
-                    mt = 0;
+        if(all_sec == 0){
+            all_sec = 1;
+            for(int a = 0; a < 5; a++){
+                if(buf[a] != com[0][1] || buf[6] != '\0'){
+                    all_sec = 0;
                     break;
                 }
             }
-            if(mt){
-                break; 
-            }
-            else{
-                fputc('S', otp);
-            }
         }
+        con = 0;
+        ch = fgetc(inp_file);
     }
-
-    fclose(inp);
-    fclose(otp);
-
+    fclose(inp_file);
+    
+    FILE *out_file = fopen(argv[2], "w");
+    if(out_file == NULL){
+        return 1;
+    }
+    for(int a = 0; a < tcon-1; a++){
+        fputc(text[a], out_file);
+    }
+    fclose(out_file);
+    
     return 0;
 }
-
